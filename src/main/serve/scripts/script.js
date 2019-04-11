a64978665366c22121ba70253a9da01da7c6478b
tkeph616 <tkeph616@students.bju.edu>
2019-04-11 19:04:03 -0400

    Implemented #19 better

--- src/main/serve/scripts/script.js
+++ src/main/serve/scripts/script.js
@@ -706,3 +706,24 @@ Judging Page
             alert(`New Result: ${verdict_name[data]}`);
         });
     }
+
+    function download(id) {
+        $(".rejudge").attr("disabled", true);
+        $(".rejudge").addClass("button-gray");
+
+        $.post("/download", {id: id}, data => {
+            $(".rejudge").attr("disabled", false);
+            $(".rejudge").removeClass("button-gray");
+            file = JSON.parse(data)
+            jQuery.each(file, (name, value) => {
+              byteCharacters = atob(value)
+              const byteNumbers = new Array(byteCharacters.length)
+              for (let i = 0; i < byteCharacters.length; i++) {
+                  byteNumbers[i] = byteCharacters.charCodeAt(i)
+              }
+              const byteArray = new Uint8Array(byteNumbers);
+              saveAs(new Blob([byteArray], {type: "application/zip"}, name))
+            })
+            
+        });
+    }
